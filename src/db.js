3012c28471793b2344589a353479d7d643daec7d
Natalia Ríos <nrios@urp.edu.pe>
2023-11-16 15:05:49 -0500

    bd
    fastt

--- src/db.js
+++ src/db.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/Pantanostest");
+        await mongoose.connect("mongodb+srv://nattriose:njRE1401@pantanos.je1d1pg.mongodb.net/?retryWrites=true&w=majority");
         console.log(">>>>>>>>DB is connected");
     } catch (error){
         console.log(error);
     }
+
 };

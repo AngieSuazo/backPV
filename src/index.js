6f864d42719eef8a906e8cc52a8fe94dbf1ec7ce
YamilEsteban1 <estebany035@gmail.com>
2023-11-23 13:30:42 -0500

    payment-pipi

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import app from './app.js'
-import {connectDB} from './db.js'
+import app from './app.js';
+import { connectDB } from './db.js';
+import { PORT } from './config.js';
 
-connectDB();
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log('Server is running on port', PORT);
+    });
+  } catch (error) {
+    console.error('Error connecting to the database:', error.message);
+  }
+};
 
-
-
-app.listen(3000);
-console.log('Server on port', 3000);
+startServer();

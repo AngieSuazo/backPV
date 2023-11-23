6decdac7e3f2e5cc98aae54b4c18485a2dedfcbe
carlosrivasesp <113138594+carlosrivasesp@users.noreply.github.com>
2023-11-23 11:43:37 -0500

    Visitante y corrección de caja

--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ import cajaRoutes from "./routes/caja.routes.js";
 
 import guiaRoutes from "./routes/guia.routes.js";
 
+import visitanteRoutes from "./routes/visitante.routes.js";
+
 import cors from "cors";
 
 const app = express();
@@ -47,5 +49,7 @@ app.use("/api", circuitoRoutes);
 app.use("/api", cajaRoutes);
 
 app.use("/api", guiaRoutes);
+
+app.use("/api", visitanteRoutes)
 
 export default app;
